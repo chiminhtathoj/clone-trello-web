f41c25c6aede1d0c6fadb4c51c83764ddf22ecb8
chiminhtathoj <32964203+chiminhtathoj@users.noreply.github.com>
2022-03-17 10:27:41 +0700

    design frontend trello

--- src/App.js
+++ src/App.js
@@ -1,9 +1,97 @@
-import './App.css';
+ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Chiminhtathoj</h1>
+    <div className="trello-master">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Name </header>
+          <ul>
+            <li>
+              <img src="https://scontent.fdad1-2.fna.fbcdn.net/v/t1.6435-9/165869891_1125869027887737_2609379682427357032_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=5IkM6up-lMQAX-MTD7Q&_nc_ht=scontent.fdad1-2.fna&oh=00_AT_UfcQqiDiCt3OIS35b184p6Bh1NT0d1mIwSGulCVs-yQ&oe=6257A3D8"
+              alt="img" />
+            </li>
+            <li>addcard</li>
+            <li>addcard</li>
+            <li>addcard</li>
+            <li>addcard</li>
+            <li>addcard</li>
+          </ul>
+          <footer>add card</footer>
+        </div>
+        <div className="column">
+          <header>Name </header>
+          <ul>
+            <li>
+              <img src="https://scontent.fdad1-2.fna.fbcdn.net/v/t1.6435-9/165869891_1125869027887737_2609379682427357032_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=5IkM6up-lMQAX-MTD7Q&_nc_ht=scontent.fdad1-2.fna&oh=00_AT_UfcQqiDiCt3OIS35b184p6Bh1NT0d1mIwSGulCVs-yQ&oe=6257A3D8"
+                alt="img" />
+            </li>
+            <li>addcard</li>
+            <li>addcard</li>
+            <li>addcard</li>
+          </ul>
+          <footer>add card</footer>
+        </div>
+        <div className="column">
+          <header>Name </header>
+          <ul>
+            <li>
+              <img src="https://scontent.fdad1-2.fna.fbcdn.net/v/t1.6435-9/165869891_1125869027887737_2609379682427357032_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=5IkM6up-lMQAX-MTD7Q&_nc_ht=scontent.fdad1-2.fna&oh=00_AT_UfcQqiDiCt3OIS35b184p6Bh1NT0d1mIwSGulCVs-yQ&oe=6257A3D8"
+              alt="img" />
+            </li>
+            <li>addcard</li>
+            <li>addcard</li>
+            <li>addcard</li>
+            <li>addcard</li>
+            <li>addcard</li>
+          </ul>
+          <footer>add card</footer>
+        </div>
+        <div className="column">
+          <header>Name </header>
+          <ul>
+            <li>
+              <img src="https://scontent.fdad1-2.fna.fbcdn.net/v/t1.6435-9/165869891_1125869027887737_2609379682427357032_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=5IkM6up-lMQAX-MTD7Q&_nc_ht=scontent.fdad1-2.fna&oh=00_AT_UfcQqiDiCt3OIS35b184p6Bh1NT0d1mIwSGulCVs-yQ&oe=6257A3D8"
+                alt="img" />
+            </li>
+            <li>addcard</li>
+            <li>addcard</li>
+            <li>addcard</li>
+          </ul>
+          <footer>add card</footer>
+        </div>
+        <div className="column">
+          <header>Name </header>
+          <ul>
+            <li>
+              <img src="https://scontent.fdad1-2.fna.fbcdn.net/v/t1.6435-9/165869891_1125869027887737_2609379682427357032_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=5IkM6up-lMQAX-MTD7Q&_nc_ht=scontent.fdad1-2.fna&oh=00_AT_UfcQqiDiCt3OIS35b184p6Bh1NT0d1mIwSGulCVs-yQ&oe=6257A3D8"
+              alt="img" />
+            </li>
+            <li>addcard</li>
+            <li>addcard</li>
+            <li>addcard</li>
+            <li>addcard</li>
+            <li>addcard</li>
+          </ul>
+          <footer>add card</footer>
+        </div>
+        <div className="column">
+          <header>Name </header>
+          <ul>
+            <li>
+              <img src="https://scontent.fdad1-2.fna.fbcdn.net/v/t1.6435-9/165869891_1125869027887737_2609379682427357032_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=5IkM6up-lMQAX-MTD7Q&_nc_ht=scontent.fdad1-2.fna&oh=00_AT_UfcQqiDiCt3OIS35b184p6Bh1NT0d1mIwSGulCVs-yQ&oe=6257A3D8"
+                alt="img" />
+            </li>
+            <li>addcard</li>
+            <li>addcard</li>
+            <li>addcard</li>
+          </ul>
+          <footer>add card</footer>
+        </div>
+
+      </div>
     </div>
   );
 }
